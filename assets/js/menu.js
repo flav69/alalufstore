026125f1cda026eb6d383f5c1cef3ba8ca036fda
Mauro Ibarra <mauroibarra133@gmail.com>
2023-01-06 17:44:23 -0300

    make the menu responsive and add JS funcionality

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,21 @@
 
 const menuButton = document.querySelector('.header-mid-menu');
 const menu = document.querySelector('.menu');
+const cruz = document.querySelector('.menu-container-cruz i');
+const nav_bg_overlay = document.querySelector('.bg-overlay');
 
-menuButton.addEventListener('click', ()=>{
+
+
+function toggleMenu(){
     menu.classList.toggle('menu-active');
-});
+    nav_bg_overlay.classList.toggle('bg-overlay-active');
+    // Bloquear o desbloquear el scroll de la página
+    if (menu.classList.contains('menu-active')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'auto';
+    }
+}
+
+menuButton.addEventListener('click',toggleMenu);
+cruz.addEventListener('click',toggleMenu);
