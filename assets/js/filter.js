d0020032008dc1c448ad599bedfe065117967ec5
Mauro Ibarra <mauroibarra133@gmail.com>
2023-01-16 17:57:55 -0300

    make the filter section responsive

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,9 +1,11 @@
 filterButton = document.querySelector('.filter-button');
 filterPage = document.querySelector('.filter-desplegable');
 filterBackButton = document.querySelector('.filter-back-button');
+//filterOverlay = document.querySelector('.filter-overlay');
+
 function toggleMenu(){
     filterPage.classList.toggle('filter-desplegable-active');
-    // carrito_bg_overlay.classList.toggle('carrito-overlay-active');
+    //filterOverlay.classList.toggle('filter-overlay-active');
     // Bloquear o desbloquear el scroll de la página
     if (filterPage.classList.contains('filter-desplegable-active')) {
         document.body.style.overflow = 'hidden';
@@ -15,4 +17,4 @@ function toggleMenu(){
 
 filterButton.addEventListener('click', toggleMenu);
 filterBackButton.addEventListener('click', toggleMenu);
-// carrito_bg_overlay.addEventListener('click',toggleMenu);
+//filterOverlay.addEventListener('click',toggleMenu);
